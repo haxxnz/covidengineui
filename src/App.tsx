4e2e4a0d8eb69a3f9d75c10f315adbe9dd53681a
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-08-20 22:59:17 +1200

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Onboarding</h1>
+
+        <h5>Check your history with your payments</h5>
+
+        <h5>Make sure you are not rekt</h5>
+
+        <h5>Maintains your privacy</h5>
       </header>
     </div>
   );

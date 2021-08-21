87457183ef93163c29b6cbbcc7b8cdc45433eeb1
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-08-21 16:09:12 +1200

    tidy up

--- src/App.tsx
+++ src/App.tsx
@@ -234,40 +234,12 @@ function CSVUpload() {
         <h1>Upload a CSV</h1>
         <div className="hr" />
         <div className="grid-2">
-          {/* <input type="file"/> */}
-
           <input type="file" name="file" onChange={changeHandler} />
           <div>
             <button onClick={handleSubmission} disabled={!isFilePicked}>
               Submit
             </button>
           </div>
-          {/* <div>
-            <h2>Connect your Bank</h2>
-            <aside>
-              We’ll check your transactions to see if you’ve been exposed to
-              COVID.
-              <br />
-              <br />
-              We don’t store any of your bank data.
-            </aside>
-            <a href="https://oauth.akahu.io/?client_id=app_token_cksl325vd000109mjaenwgicd&response_type=code&redirect_uri=https://oauth.covidengine.ml/auth/akahu&scope=ENDURING_CONSENT">
-              <button className="primary">Connect your Bank</button>
-            </a>
-          </div>
-          <div>
-            <h2>Upload a CSV</h2>
-            <aside>
-              You can find this by logging into your bank on your computer -
-              it’s a spreadsheet you can download.
-              <br />
-              <br />
-              We don’t store any of your bank data.
-            </aside>
-            <Link to="/csv">
-              <button className="primary">Upload CSV</button>
-            </Link>
-          </div> */}
         </div>
       </section>
     </div>
